3a3bbd54c4f4fdbd8ab607fd05e247f624cff8fa
Saber13110 <elouakilisaber@gmail.com>
2025-06-12 16:16:48 -1000

    Enable CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import auth, tracking
 from .auth import get_current_user
@@ -6,6 +7,16 @@ from .auth import get_current_user
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["http://localhost:4200"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Inject dependency for /api/auth/me to use token validation
 app.include_router(auth.router, prefix='/api/auth')
